76212ddb79177e0e1837c762183fc3c2342ad087
Abdelilah Fakhour <Abdelilah.Fakhour@outlook.fr>
2023-05-01 13:26:24 +0200

    modif detailtpoic

--- view/forum/detailTopic.php
+++ view/forum/detailTopic.php
@@ -7,10 +7,10 @@ $posts = $result["data"]['posts'];
 
 <h1 style="text-align: center; color: white; margin-bottom: 30px">Listes des posts</h1>
 
-<button class="btn btn-dark" style="display: flex;justify-content: center; margin: auto; margin-bottom: 30px;"><a style="color: white;" href="index.php?action=addPost">ADD Realisateur</a></button>
+<button class="btn btn-dark" style="display: flex;justify-content: center; margin: auto; margin-bottom: 30px;"><a style="color: white;" href="index.php?action=addPost">New Post</a></button>
 
 <?php foreach($posts as $post){
-var_dump($posts);
+// var_dump($posts);
     ?>
 
 
