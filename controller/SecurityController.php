2f05c5436ef5acb90e91d20d385e2ed58cf0cfc7
Abdelilah Fakhour <Abdelilah.Fakhour@outlook.fr>
2023-05-01 13:25:51 +0200

    ajout fonction register

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -29,8 +29,38 @@
             if(!empty($_POST)){
 
                 $nickname = filter_input(INPUT_POST, 'nickname', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-                $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-                $mail = filter_input(INPUT_POST, 'mail', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-            }
+                $password = filter_input(INPUT_POST, 'password', FILTER_VALIDATE_REGEXP, array(
+                    "options" => array("regexp" => '/[A-Za-z0-9]{12,32}/')
+                ));
+                $confimrPassword = filter_input(INPUT_POST, 'confirmPassword', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                $mail = filter_input(INPUT_POST, 'mail', FILTER_SANITIZE_FULL_SPECIAL_CHARS, FILTER_VALIDATE_EMAIL);
+
+                if($nickname && $password && $mail){
+
+                    if(($password == $confimrPassword) and strlen($password) >= 12){
+
+                        $manager = new UserManager();
+                        $user = $manager->findOneByNickname($nickname);
+
+                        if(!$user){
+
+                            $hash = password_hash($password, PASSWORD_DEFAULT);
+
+                            if($manager->add([
+                                "nickname" => $nickname,
+                                "mail" => $mail, 
+                                "password" => $hash,
+                            ]));
+                        }
+                    }
+                }
+            } return [
+                "view" => VIEW_DIR."security/register.php"
+            ];
+        }
+
+        public function login(){
+
+            
         }
     }
