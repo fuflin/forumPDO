6af2a3516afb5a71349731fb116204afb68806e6
Abdelilah Fakhour <Abdelilah.Fakhour@outlook.fr>
2023-05-02 08:31:59 +0200

    creation formulaire ajout catégorie

---
+++ view/forum/addCat.php
@@ -0,0 +1,15 @@
+<h1 style="text-align: center; color: white; margin-bottom: 30px;">Bienvenue sur la page Ajouter category</h1>
+
+<h2 style="text-align: center; color: white; margin-bottom: 30px;">Ajouter Réalisateur</h2>
+
+
+    
+<form action="index.php?ctrl=forum&action=addCat" method="post">
+    
+    <input type="text" name="name" placeholder="name">
+
+    <input type="text" name="img" placeholder="image">
+
+    <button type="submit">Confirm</button>
+
+</form>
