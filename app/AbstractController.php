2352d3783445a824e6c8b0aa0911835ea442cbb8
Abdelilah Fakhour <Abdelilah.Fakhour@outlook.fr>
2023-05-03 19:30:00 +0200

    modif pour une fonction

--- app/AbstractController.php
+++ app/AbstractController.php
@@ -6,18 +6,14 @@
         public function index(){}
         
         public function redirectTo($ctrl = null, $action = null, $id = null){
-
-            if($ctrl != "home"){
-                $url = $ctrl ? "/".$ctrl : "";
-                $url.= $action ? "/".$action : "";
-                $url.= $id ? "/".$id : "";
-                $url.= ".html";
-            }
-            else $url = "/";
-            header("Location: $url");
-            die();
-
-        }
+        if ($ctrl != "home") {
+            $url = $ctrl ? "?ctrl=" . $ctrl : "";
+            $url .= $action ? "&action=" . $action : "";
+            $url .= $id ? "&id=" . $id : "";
+        } else $url = "/";
+        header("Location: $url");
+        die();
+    }
 
         public function restrictTo($role){
             
